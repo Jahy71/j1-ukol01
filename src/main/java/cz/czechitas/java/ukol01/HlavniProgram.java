ba4dd3a795fb5e6f9e84ac28a7b51792cf34fc20
Jahy71 <jiri.jahoda@email.cz>
2023-02-18 15:08:44 +0100

    Část 2 - Objekty

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -4,7 +4,7 @@ import cz.czechitas.java.ukol01.engine.*;
 
 public class HlavniProgram {
 
-    public void start() {
+    public void nakresliPrasatko() {
         Turtle zofka = new Turtle();
 
         for (int i = 0; i < 2; i++) {
@@ -13,14 +13,12 @@ public class HlavniProgram {
             zofka.move(200);
             zofka.turnRight(90);
         }
-
         zofka.turnLeft(45);
         zofka.move(70);
         zofka.turnRight(90);
         zofka.move(70);
         zofka.penUp();
         zofka.turnRight(135);
-
         zofka.penDown();
         zofka.move(100);
         zofka.turnRight(25);
@@ -41,13 +39,61 @@ public class HlavniProgram {
         zofka.move(40);
         zofka.turnLeft(140);
         zofka.move(40);
+    }
 
+    public void osmiuhelnik() {
+        Turtle zofka = new Turtle();
 
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(150);
+        zofka.penDown();
+
+        for (int i = 0; i < 8; i++) {
+            zofka.move(35);
+            zofka.turnLeft(45);
+            zofka.move(35);
+        }
+    }
+
+    public void kolecko() {
+        Turtle zofka = new Turtle();
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(300);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.penDown();
+
+        for (int i = 0; i < 18; i++) {
+            zofka.move(30);
+            zofka.turnLeft(20);
+        }
+    }
+
+    public void slunicko() {
+        Turtle zofka = new Turtle();
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(250);
+        zofka.penDown();
+
+        for (int i = 0; i < 18; i++) {
+            zofka.turnRight(90);
+            zofka.move(35);
+            zofka.turnRight(180);
+            zofka.move(35);
+            zofka.turnRight(90);
+            zofka.move(30);
+            zofka.turnLeft(20);
+        }
 
     }
 
     public static void main(String[] args) {
-        new HlavniProgram().start();
+        new HlavniProgram().nakresliPrasatko();
+        new HlavniProgram().osmiuhelnik();
+        new HlavniProgram().kolecko();
+        new HlavniProgram().slunicko();
     }
-
 }
